91b57d85904e4bb088a57cf0bdff49a15c460097
nikki <waistperfection@yandx.ru>
2024-04-23 20:37:49 +0300

    first domain model and tests

---
+++ model.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from datetime import date
+
+from exceptinos import OutOfStock
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: date| None = None):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+    
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+    
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+    
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def __eq__(self, other)->bool:
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+    
+    def __hash__(self) -> int:
+        return hash(self.reference)
+    
+    def __gt__(self, other: "Batch") -> bool:
+        """
+        use to sort items of Batch by shipment time and 'in_stock' first
+        """
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+    
+def allocate(line: OrderLine, batches: list[Batch]) -> str:
+    
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+    except StopIteration:
+        raise OutOfStock(f"Нет в наличии {line.sku}")
+    else:
+        return batch.reference
